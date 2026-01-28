e29443000b84fdc2537ce3e4a8be0348e80b6475
KI <khoitr190@gmail.com>
2026-01-28 12:59:39 +0700

    feat: hiden files

--- check-fee-mint.js
+++ check-fee-mint.js
@@ -2,8 +2,7 @@ const dotenv = require("dotenv");
 const axios = require("axios");
 dotenv.config();
 
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhmY2x5emFkYWl2amx3aHhjbmNqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTIwMzY4MiwiZXhwIjoyMDQ2Nzc5NjgyfQ.mhHLf9b0APfemC28egGi9bae5RAQ69LntZrnT5eBPe4";
+const supabaseKey = ""
 
 async function getFeeMint() {
   try {
